21127313c3a77b01d9d4421a0850371ba7facc09
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-05-03 12:52:30 +0100

    WebView in Kotlin

---
+++ WebViewInKotlin/app/src/main/java/com/dodemy/android/webviewinkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.dodemy.android.webviewinkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pageUrl = "https://dodemy.com/"
+        launchWebView.setOnClickListener {
+            val intent = WebActivity.newIntent(this, pageUrl)
+            startActivity(intent)
+        }
+    }
+
+}
