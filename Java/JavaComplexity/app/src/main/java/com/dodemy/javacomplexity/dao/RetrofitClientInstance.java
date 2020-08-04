aa21eb0cdf9c8cfef115a357309e6759d83dbe30
maduoma <18477889+maduoma@users.noreply.github.com>
2020-08-05 00:25:26 +0100

    Initial Commit

--- Java/JavaComplexity/app/src/main/java/com/dodemy/javacomplexity/dao/RetrofitClientInstance.java
+++ Java/JavaComplexity/app/src/main/java/com/dodemy/javacomplexity/dao/RetrofitClientInstance.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
 
-    private  static Retrofit retrofit;
+    private static Retrofit retrofit;
     private static final String BASE_URL = "https://raw.githubusercontent.com";
 
     public static Retrofit getRetrofitInstance() {
