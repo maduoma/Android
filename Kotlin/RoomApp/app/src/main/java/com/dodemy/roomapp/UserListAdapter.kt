4ff9aa0882e90a1741cf81cae117074088fa45b8
maduoma <18477889+maduoma@users.noreply.github.com>
2020-08-29 23:41:37 +0100

    Initial Commit

--- Kotlin/RoomApp/app/src/main/java/com/dodemy/roomapp/UserListAdapter.kt
+++ Kotlin/RoomApp/app/src/main/java/com/dodemy/roomapp/UserListAdapter.kt
@@ -3,6 +3,9 @@ package com.dodemy.roomapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.layout_user_list.view.*
@@ -51,11 +54,11 @@ class UserListAdapter : RecyclerView.Adapter<MyViewHolder>() {
 
 class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-    val username = view.text_username
-    val location = view.text_location
-    val email = view.text_email
-    val layout = view.layout
-    val imgDelete = view.imgDelete
+    val username: TextView = view.text_username
+    val location: TextView = view.text_location
+    val email: TextView = view.text_email
+    val layout: ConstraintLayout = view.layout
+    val imgDelete: ImageView = view.imgDelete
 }
 
 
