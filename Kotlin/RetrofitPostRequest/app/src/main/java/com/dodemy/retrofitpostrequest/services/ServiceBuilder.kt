9a94781c6a9aff57f6b1e98a6b1e0cef23d6f5e9
maduoma <18477889+maduoma@users.noreply.github.com>
2020-08-30 18:58:13 +0100

    Initial Commit

--- Kotlin/RetrofitPostRequest/app/src/main/java/com/dodemy/retrofitpostrequest/services/ServiceBuilder.kt
+++ Kotlin/RetrofitPostRequest/app/src/main/java/com/dodemy/retrofitpostrequest/services/ServiceBuilder.kt
@@ -3,7 +3,6 @@ package com.dodemy.retrofitpostrequest.services
 import android.os.Build
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
