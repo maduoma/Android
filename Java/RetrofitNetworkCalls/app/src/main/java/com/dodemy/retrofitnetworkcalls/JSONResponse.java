e5b7cef32cdb276d4afc7f04819163548c22ba90
maduoma <18477889+maduoma@users.noreply.github.com>
2020-09-01 10:20:59 +0100

    Initial commit

--- Java/RetrofitNetworkCalls/app/src/main/java/com/dodemy/retrofitnetworkcalls/JSONResponse.java
+++ Java/RetrofitNetworkCalls/app/src/main/java/com/dodemy/retrofitnetworkcalls/JSONResponse.java
@@ -3,7 +3,19 @@ package com.dodemy.retrofitnetworkcalls;
 public class JSONResponse {
     private AndroidVersion[] android;
 
+    public JSONResponse(AndroidVersion[] android) {
+        this.android = android;
+    }
+
     public AndroidVersion[] getAndroid() {
         return android;
     }
+
+    /*
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+     */
 }
