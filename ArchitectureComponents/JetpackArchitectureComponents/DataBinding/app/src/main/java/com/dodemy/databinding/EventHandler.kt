d87c42503b293178d00435abac896b76e7a2a523
maduoma <18477889+maduoma@users.noreply.github.com>
2020-09-24 13:57:58 +0100

    Jetpack Architecture Components - Data Binding

--- ArchitectureComponents/JetpackArchitectureComponents/DataBinding/app/src/main/java/com/dodemy/databinding/EventHandler.kt
+++ ArchitectureComponents/JetpackArchitectureComponents/DataBinding/app/src/main/java/com/dodemy/databinding/EventHandler.kt
@@ -6,6 +6,6 @@ import android.widget.Toast
 open class EventHandler(context: Context) {
     private val myContext: Context = context
     fun eventHandler() {
-        Toast.makeText(myContext, "Welcome to Data Binding", Toast.LENGTH_LONG).show()
+        Toast.makeText(myContext, "Welcome to Data Binding!", Toast.LENGTH_LONG).show()
     }
 }
