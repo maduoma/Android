93181ad4fb52316fc78ce10186a04f5d8e45d742
maduoma <18477889+maduoma@users.noreply.github.com>
2020-09-24 14:10:18 +0100

    Jetpack Architecture Components - Data Binding

--- ArchitectureComponents/JetpackArchitectureComponents/DataBinding/app/src/main/java/com/dodemy/databinding/MainActivity.kt
+++ ArchitectureComponents/JetpackArchitectureComponents/DataBinding/app/src/main/java/com/dodemy/databinding/MainActivity.kt
@@ -8,23 +8,30 @@ import com.dodemy.databinding.databinding.ActivityMainBinding
 /**
  * @author Maduabughichi Achilefu
  * Layout and Binding expressions
+ * binding is the instance variable of ActivityMainBinding
  */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Replaces & sets setContentView(R.layout.activity_main) with Data Binding
+        //ActivityMainBinding is created automatically from the XML layout file
+        //If ActivityMainBinding is not showing as you type, rebuild and/or Invalidate/Restart Android Studio
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //Binds views and widgets using Data Binding
+        //name1 & email1 are the name variables in the XML file (activity_main.xml)
+        //If name1 in getString(R.string.name1) is highlighted red, rebuild to make it disappear
         binding.name1.text = getString(R.string.name1)
         binding.email1.text = getString(R.string.email1)
 
-        //Binds data objects with views
+        //Binds data objects class [Contact(var myName: String, var myEmail: String)] with views
+        //contact is the name variable in the XML file (activity_main.xml)
         binding.contact = Contact("Maduabughichi Achilefu2", "email1@gmail.com2")
 
-        //Event handling with Data Binding
+        //handler is the name variable in the XML file (activity_main.xml)
+        //Event handling [open class EventHandler(context: Context)] with Data Binding
         binding.handler = EventHandler(this)
 
     }
