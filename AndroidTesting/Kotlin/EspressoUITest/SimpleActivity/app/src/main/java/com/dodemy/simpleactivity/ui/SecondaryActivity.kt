ac0626cb11c43240630d778e7e68b25d15f93b04
maduoma <18477889+maduoma@users.noreply.github.com>
2020-10-08 12:25:43 +0100

    Modified SecondaryActivity

--- AndroidTesting/Kotlin/EspressoUITest/SimpleActivity/app/src/main/java/com/dodemy/simpleactivity/ui/SecondaryActivity.kt
+++ AndroidTesting/Kotlin/EspressoUITest/SimpleActivity/app/src/main/java/com/dodemy/simpleactivity/ui/SecondaryActivity.kt
@@ -1,7 +1,8 @@
 package com.dodemy.simpleactivity.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dodemy.simpleactivity.R
 import kotlinx.android.synthetic.main.activity_secondary.*
 
@@ -11,6 +12,11 @@ class SecondaryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_secondary)
         button_back.setOnClickListener {
             onBackPressed()
+        }
+
+        goToThirdActivity.setOnClickListener {
+            val thirdActivity = Intent(this, ThirdActivity::class.java)
+            startActivity(thirdActivity)
         }
     }
 }
