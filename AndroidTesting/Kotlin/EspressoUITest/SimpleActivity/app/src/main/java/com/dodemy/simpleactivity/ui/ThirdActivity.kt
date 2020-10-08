63fdac2da22bb89d5ac1400c8aac894c56e4deb3
maduoma <18477889+maduoma@users.noreply.github.com>
2020-10-08 12:28:06 +0100

    Added ThirdActivity.kt

---
+++ AndroidTesting/Kotlin/EspressoUITest/SimpleActivity/app/src/main/java/com/dodemy/simpleactivity/ui/ThirdActivity.kt
@@ -0,0 +1,17 @@
+package com.dodemy.simpleactivity.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dodemy.simpleactivity.R
+import kotlinx.android.synthetic.main.activity_third.*
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        backToThird.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
