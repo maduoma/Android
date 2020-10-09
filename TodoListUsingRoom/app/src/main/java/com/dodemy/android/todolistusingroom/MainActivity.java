c08cb935dde894b2b389524bafba7ed370fc3a2a
maduoma <18477889+maduoma@users.noreply.github.com>
2020-10-09 06:59:55 +0100

    TodoList Using Room

--- TodoListUsingRoom/app/src/main/java/com/dodemy/android/todolistusingroom/MainActivity.java
+++ TodoListUsingRoom/app/src/main/java/com/dodemy/android/todolistusingroom/MainActivity.java
@@ -10,11 +10,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
