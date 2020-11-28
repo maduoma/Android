6950ca2762f07265251212e7f1dbfad9aa209baa
maduoma <18477889+maduoma@users.noreply.github.com>
2020-11-28 16:56:52 +0100

    Room - NoteKeeper

--- ArchitectureComponents/JetpackArchitectureComponents/RoomNoteKeeper/app/src/main/java/com/dodemy/room_notekeeper/MainActivity.kt
+++ ArchitectureComponents/JetpackArchitectureComponents/RoomNoteKeeper/app/src/main/java/com/dodemy/room_notekeeper/MainActivity.kt
@@ -2,10 +2,14 @@ package com.dodemy.room_notekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(private val NoteViewModelFactory: ViewModelProvider.Factory) : AppCompatActivity() {
+    private lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProvider(this, NoteViewModelFactory).get(NoteViewModel::class.java)
     }
 }
---
+++ ArchitectureComponents/JetpackArchitectureComponents/RoomNoteKeeper/app/src/main/java/com/dodemy/room_notekeeper/NoteViewModel.kt
@@ -0,0 +1,9 @@
+package com.dodemy.room_notekeeper
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+
+//GameViewModel : ViewModel()
+class NoteViewModel : ViewModel() {
+}
