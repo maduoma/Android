9bb31f6effa75901851f1501780403a8f4b15aa0
maduoma <18477889+maduoma@users.noreply.github.com>
2020-11-29 11:51:20 +0100

    Room - ToDo

---
+++ ArchitectureComponents/JetpackArchitectureComponents/RoomToDo/app/src/main/java/com/dodemy/room_todo/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,44 @@
+package com.dodemy.room_todo.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dodemy.room_todo.MyApp
+import com.dodemy.room_todo.model.ToDo
+import com.dodemy.room_todo.repository.ToDoRepository
+import com.dodemy.room_todo.repository.ToDoRoomDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel: ViewModel()
+{
+    private val toDoRepository: ToDoRepository
+
+    private val listOfToDosLiveData: LiveData<List<ToDo>>
+
+    init
+    {
+        val toDoDao = ToDoRoomDatabase.getDatabase(MyApp.applicationContext()).toDoDao()
+        toDoRepository = ToDoRepository(toDoDao)
+        listOfToDosLiveData = toDoRepository.allToDo
+    }
+
+    fun addToDo(toDo: ToDo)
+    {
+        viewModelScope.launch (Dispatchers.IO) {
+            toDoRepository.insertToDo(toDo)
+        }
+    }
+    fun updateToDo(toDo: ToDo)
+    {
+        viewModelScope.launch (Dispatchers.IO) {
+            toDoRepository.updateToDo(toDo)
+        }
+    }
+    fun deleteToDo(toDo: ToDo)
+    {
+        viewModelScope.launch (Dispatchers.IO) {
+            toDoRepository.deleteToDo(toDo)
+        }
+    }
+}
