80a82a981a63430a60903c65cf627b25c837a24f
maduoma <18477889+maduoma@users.noreply.github.com>
2021-01-19 10:07:00 +0100

    To Do List With Room

---
+++ ArchitecturalPatterns/MVVM/ToDoListWithRoom/app/src/main/java/com/dodemy/todolistwithroom/AddTaskViewModel.java
@@ -0,0 +1,32 @@
+package com.dodemy.todolistwithroom;
+
+import com.dodemy.todolistwithroom.database.AppDatabase;
+import com.dodemy.todolistwithroom.database.TaskEntry;
+import com.dodemy.todolistwithroom.database.TasksRepository;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AddTaskViewModel extends ViewModel {
+
+    // COMPLETED (6) Add a task member variable for the TaskEntry object wrapped in a LiveData
+    private LiveData<TaskEntry> task;
+    private final TasksRepository tasksRepository;
+
+    // COMPLETED (8) Create a constructor where you call loadTaskById of the taskDao to initialize the tasks variable
+    // Note: The constructor should receive the database and the taskId
+    public AddTaskViewModel(AppDatabase database, int taskId) {
+       // task = database.taskDao().loadTaskById(taskId);
+        tasksRepository = new TasksRepository(database);
+        task = tasksRepository.getloadTaskById(taskId);
+    }
+
+    // COMPLETED (7) Create a getter for the task variable
+    public LiveData<TaskEntry> getTask() {
+        return task;
+    }
+
+    public void updateTask(TaskEntry task) {
+        tasksRepository.updateTaskById(task);
+    }
+}
---
+++ ArchitecturalPatterns/MVVM/ToDoListWithRoom/app/src/main/java/com/dodemy/todolistwithroom/database/TasksRepository.java
@@ -0,0 +1,36 @@
+package com.dodemy.todolistwithroom.database;
+
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
+public class TasksRepository {
+    private static final String LOG_TAG = TasksRepository.class.getSimpleName();
+    private LiveData<List<TaskEntry>> tasks;
+    private TaskDao taskDao;
+    AppDatabase database;
+
+    public TasksRepository(AppDatabase database) {
+        this.database = database;
+    }
+
+
+    public LiveData<List<TaskEntry>> getloadAllTasks() {
+        tasks = database.taskDao().loadAllTasks();
+        return tasks;
+    }
+
+
+    public LiveData<TaskEntry> getloadTaskById(int taskId) {
+        return database.taskDao().loadTaskById(taskId);
+    }
+
+    public void deleteTasks(TaskEntry taskEntry) {
+        database.taskDao().deleteTask(taskEntry);
+    }
+
+    public void updateTaskById(TaskEntry task) {
+        database.taskDao().updateTask(task);
+    }
+}
